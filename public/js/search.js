03130704acd6259940dda4446ad66cf1dc9c2ed4
rjainsobha-debug <rjain.sobha@gmail.com>
2026-04-18 23:21:38 +0530

    Create search.js

---
+++ public/js/search.js
@@ -0,0 +1,10 @@
+document.getElementById('search').addEventListener('keyup', async (e) => {
+  const q = e.target.value;
+
+  if (q.length < 2) return;
+
+  const res = await fetch(`/api/search?q=${q}`);
+  const data = await res.json();
+
+  console.log(data);
+});
